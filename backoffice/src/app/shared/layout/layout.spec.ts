d397e08f9e0bd0a651425ac7c6feb639c1744452
Wiame Jaoui <wiamejaoui.wj@gmail.com>
2026-08-26 14:50:17 +0200

    Fix broken backoffice unit tests

--- backoffice/src/app/shared/layout/layout.spec.ts
+++ backoffice/src/app/shared/layout/layout.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { Layout } from './layout';
 
@@ -9,6 +10,7 @@ describe('Layout', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Layout],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Layout);
